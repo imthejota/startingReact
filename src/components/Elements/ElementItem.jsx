d6452f904f3d8b318649e6d0e60df27823e0fe26
Jonathan Kechian <jkechian@gmail.com>
2023-06-27 15:14:35 -0300

    fix syntax and styling

--- src/components/Elements/ElementItem.jsx
+++ src/components/Elements/ElementItem.jsx
@@ -1,12 +1,11 @@
-import Card from "../UI/Card"
-
+import Card from "../UI/Card";
 
 export default function ElementItem(props) {
     return (
         <Card>
-                <img src={props.image} alt={props.title} />
-                <h2>{props.title}</h2>
-                <p>{props.description}</p>
+            <img src={props.image} alt={props.title} />
+            <h2>{props.title}</h2>
+            <p>{props.description}</p>
         </Card>
-    )
+    );
 }
